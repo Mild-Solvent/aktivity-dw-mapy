0ae123c6ae1f7af0e53e9048de71047133393ea5
Mild-Solvent <matejcik.denis@gmail.com>
2025-10-09 02:22:02 +0300

    Fix GitHub Pages workflow: Add package-lock.json and update paths

--- aktivity-dw-mapy-next/dist/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"n42IMgVdDC8SszsBe4zs6dDZsejxRo4FLQUcSJqkcsA=\"\n}"
--- aktivity-dw-mapy-next/dist/static/chunks/app_track_[id]_page_tsx_2bd2f040._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_b6af755d._.js",
-  "static/chunks/_85eca306._.js"
-],
-    source: "dynamic"
-});
