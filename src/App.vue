85435641583e8ae58415b493b44d349e8a31ceb0
Mild-Solvent <matejcik.denis@gmail.com>
2025-10-13 21:56:11 +0300

    link in footah

--- src/App.vue
+++ src/App.vue
@@ -142,9 +142,9 @@
             <div class="developer">
               Development a design od <a href="https://mild-solvent.github.io/Portfolio/" target="_blank" rel="noopener" class="developer-link">Mild Solvent</a>
             </div>
-            <div class="footer-cea-flower">
+            <a href="https://new.ceaeurope.sk/" target="_blank" rel="noopener" class="footer-cea-flower">
               <img src="/assets/shared/cea-flower.png" alt="CEA Flower" class="cea-flower-logo">
-            </div>
+            </a>
           </div>
         </div>
       </div>
